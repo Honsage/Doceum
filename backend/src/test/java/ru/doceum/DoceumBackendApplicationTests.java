68c2ba4a5c526ed175eb5a45da13e947d70fae47
Honsage <musatov.i.a@edu.mirea.ru>
2026-04-26 02:19:07 +0300

    feat(backend): create raw structure of spring application

---
+++ backend/src/test/java/ru/doceum/DoceumBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.doceum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoceumBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
