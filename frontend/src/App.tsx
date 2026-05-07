cf1360bcc13c63b1cf9c01c752e0c33465de2056
Honsage <musatov.i.a@edu.mirea.ru>
2026-05-07 18:23:08 +0300

    feat(frontend): configure structure of routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-    </>
-  )
+      <Routes>
+        // Pages with standard layout
+        <Route element={null}>
+          <Route path="/" element={null} />
+        </Route>
+      </Routes>
+  );
 }
 
-export default App
+export default App;
