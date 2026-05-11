f276f7b90ba6c798956236a2d1278f82942cd962
Honsage <musatov.i.a@edu.mirea.ru>
2026-05-12 02:21:44 +0300

    build(frontend): configure vite to handle wasm

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import wasm from 'vite-plugin-wasm';
+import topLevelAwait from 'vite-plugin-top-level-await';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    wasm(),
+    topLevelAwait()
+  ],
+  
   server: {
     port: 3000,
     open: true,
     host: '0.0.0.0',
     strictPort: true,
   },
+  
   resolve: {
     alias: {
       '@assets': path.resolve(__dirname, './src/assets'),
     }
+  },
+  
+  optimizeDeps: {
+    exclude: ['doceum-wasm']
   }
 })
