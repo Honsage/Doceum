e36f10156de9625df122396eeffedda81930f80f
Honsage <musatov.i.a@edu.mirea.ru>
2026-05-13 19:24:52 +0300

    build(backend): register auth and documents modules in main class

--- backend/src/main/java/ru/doceum/DoceumBackendApplication.java
+++ backend/src/main/java/ru/doceum/DoceumBackendApplication.java
@@ -2,11 +2,17 @@ package ru.doceum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import ru.doceum.modules.auth.AuthModuleConfig;
+import ru.doceum.modules.documents.DocumentsModuleConfig;
 
 @SpringBootApplication
+@Import({
+        AuthModuleConfig.class,
+        DocumentsModuleConfig.class
+})
 public class DoceumBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(DoceumBackendApplication.class, args);
     }
-
 }
