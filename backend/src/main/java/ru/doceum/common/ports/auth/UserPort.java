2e6f6a0d7ecfd7e96747418b0953d6275f5f3d89
Honsage <musatov.i.a@edu.mirea.ru>
2026-05-14 22:48:31 +0300

    feat(backend/common): update user port contract with searching users by full name

--- backend/src/main/java/ru/doceum/common/ports/auth/UserPort.java
+++ backend/src/main/java/ru/doceum/common/ports/auth/UserPort.java
@@ -1,9 +1,10 @@
 package ru.doceum.common.ports.auth;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserPort {
     Optional<UserInfo> getUserInfo(UUID userId);
+    List<UserInfo> findUsersByFullNameLike(String query);
 }
-
