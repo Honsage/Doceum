ab604538aeb421886f2923b1f44bdf46fb214d47
Honsage <musatov.i.a@edu.mirea.ru>
2026-05-14 23:14:42 +0300

    feat(backend/hub): configure hub module

---
+++ backend/src/main/java/ru/doceum/modules/hub/HubModuleConfig.java
@@ -0,0 +1,9 @@
+package ru.doceum.modules.hub;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackageClasses = HubModuleConfig.class)
+public class HubModuleConfig {
+}
