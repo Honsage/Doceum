570b80bef414e7697234b46149edb427810ded25
Honsage <musatov.i.a@edu.mirea.ru>
2026-05-14 23:15:29 +0300

    build(backend): register hub module in main class

--- backend/src/main/java/ru/doceum/DoceumBackendApplication.java
+++ backend/src/main/java/ru/doceum/DoceumBackendApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 import ru.doceum.modules.auth.AuthModuleConfig;
 import ru.doceum.modules.documents.DocumentsModuleConfig;
+import ru.doceum.modules.hub.HubModuleConfig;
 
 @SpringBootApplication
 @Import({
         AuthModuleConfig.class,
-        DocumentsModuleConfig.class
+        DocumentsModuleConfig.class,
+        HubModuleConfig.class
 })
 public class DoceumBackendApplication {
     public static void main(String[] args) {
