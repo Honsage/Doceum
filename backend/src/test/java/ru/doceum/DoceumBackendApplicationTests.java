06ec4e177343970e6db040c9c08647458bd49955
Honsage <musatov.i.a@edu.mirea.ru>
2026-05-18 01:50:57 +0300

    test(backend): configure testing and create test environment

--- backend/src/test/java/ru/doceum/DoceumBackendApplicationTests.java
+++ backend/src/test/java/ru/doceum/DoceumBackendApplicationTests.java
@@ -2,12 +2,13 @@ package ru.doceum;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DoceumBackendApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
